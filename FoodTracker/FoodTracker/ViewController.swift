d5d5a931666a6e9ba14bf144295882f54c8cbf8b
“wheatandcat” <wheatandcat@gmail.com>
2017-09-09 23:10:16 +0900

    step 1

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by wheatandcat on 2017/09/09.
+//  Copyright © 2017年 wheatandcat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
